c1288f10b91b4be8063bb727d4f251e84e749e49
Somesh.Musunuri <Somesh.Musunuri@ril.com>
2019-03-03 20:01:18 +0530

    Updates

---
+++ precache-manifest.fde4f4c41bded3f1d15f78ba3ad97990.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3b7fa050b768968df1c",
+    "url": "/rss-feed/static/js/runtime~main.f3b7fa05.js"
+  },
+  {
+    "revision": "d7c81488d73420881d96",
+    "url": "/rss-feed/static/js/main.d7c81488.chunk.js"
+  },
+  {
+    "revision": "c532a25114f4bbc124ca",
+    "url": "/rss-feed/static/js/2.c532a251.chunk.js"
+  },
+  {
+    "revision": "d7c81488d73420881d96",
+    "url": "/rss-feed/static/css/main.8ef68682.chunk.css"
+  },
+  {
+    "revision": "72e9fa219004e47e8815c3481ad90734",
+    "url": "/rss-feed/index.html"
+  }
+];
